5d8d38af39e6e7ffe898b7e5f1830b7f432d394d
lola0206 <lcmontoyaa@unal.edu.co>
2020-10-29 15:55:35 -0500

    [Feat] Libreria dataset salaries

--- salaries_lib/__init__.py
+++ salaries_lib/__init__.py
@@ -1,2 +1,4 @@
 from .fsalaries import mayus 
 from .fsalaries import download_data
+from .fsalaries import sel
+from .fsalaries import divide
--- salaries_lib/fsalaries.py
+++ salaries_lib/fsalaries.py
@@ -4,9 +4,13 @@ import pathlib
 
 
 def mayus(salaries_df):
-    """Función mayus:convierte los nombres de las columnas del dataset a mayúsculas
-    Parametro
-    Argumento"""
+    #Convierte los nombres de las columnas del dataset a mayúsculas
+    
+    #Parametros: 
+    # salaries_df (dataframe): el data frame para el cual se necesita modificar los nombres de las columnas 
+    
+    #Regresa:
+    # salaries_df (dataframe): el dataframe modificado
 
     salaries_df.columns = salaries_df.columns.str.upper()
     return(salaries_df)
@@ -14,7 +18,14 @@ def mayus(salaries_df):
 
 
 def download_data(path_file):
-    """Función download_data: Descarga el dataset salaries desde Github hasta la carpeta especificada por el usuario"""
+    #Descarga el dataset salaries desde Github hasta la carpeta especificada por el usuario
+
+    #Parametros:
+    #path_file (str): ruta de almacenamiento del archivo, junto con su nombre
+    
+    #Regresa:
+    #path_file (str): la ruta donde fue almacenado con éxito el archivo junto con el nombre dle mismo
+
     location = pathlib.Path(path_file)
     folder = location.parent
     pathlib.Path(folder).mkdir(exist_ok=True)
@@ -24,3 +35,35 @@ def download_data(path_file):
         f.write(r.content)
     return str(path_file)
 
+
+
+def sel(salaries_df):
+    #Crea  un dataframe con las columnas necesarias para comenzar la limpieza
+
+    #Parametros: 
+    #salaries_df (dataframe)
+
+    #Regresa: 
+    #salaries_df (dataframe): dataframe con las columnas seleccionadas 
+
+    salaries_df = salaries_df[["¿A usted le pagan en pesos colombianos (COP) o dólares (USD)?",
+                                    "¿Usando la moneda de la respuesta anterior, cuánto es su remuneración ANUAL base?  eg 36,000,000 si es pesos o 3,600 si es dólares"]]
+    return(salaries_df)
+
+
+
+def divide(salaries_df):
+    #Crea  dos data frame separados para analizar quienen ganan en pesos y dólares
+
+
+    #Parametros: 
+    #salaries_df (dataframe)
+
+    #Regresa: 
+    #salaries_df_pesos (dataframe): dataframe con valores en peso
+    #salaries_df_dolar (dataframe): dataframe con valores en peso
+
+    salaries_df_pesos =  salaries_df.loc[salaries_df["¿A USTED LE PAGAN EN PESOS COLOMBIANOS (COP) O DÓLARES (USD)?"]== "pesos",:]
+    salaries_df_dolar =  salaries_df.loc[salaries_df["¿A USTED LE PAGAN EN PESOS COLOMBIANOS (COP) O DÓLARES (USD)?"]== "dólares",:]
+
+    return(salaries_df_pesos,salaries_df_dolar)
